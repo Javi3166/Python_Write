1a816c447addcc34b783a49692f791d8fe2b272c
Emilio <javi3166@yahoo.com>
2024-11-01 14:19:04 -0400

    Initial commit

---
+++ Main.py
@@ -0,0 +1,14 @@
+employee_file = open("Employees.txt", "a")
+
+employee_file.write("Toby - Human Resources") #It attaches as is to the latest line, so if there is no empty new line,
+                                                # it'll attach right after the last one
+
+employee_file.write("\nKelly - Customer Service")
+
+employee_file.close()
+
+employee_file = open("Employees_Another.txt", "w")
+
+employee_file.write("Toby - Human Resources")
+
+employee_file.close()
